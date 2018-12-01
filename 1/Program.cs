e5c58772623fe188809f8e235b344317ed14be62
Christian Wattengård <cwattengard@gmail.com>
2018-12-01 21:41:56 +0100

    It works... Barely...

---
+++ 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = File.ReadAllLines("input").Select(q => int.Parse(q)).ToList();
+            Console.WriteLine("Sum");
+            Console.WriteLine(data.Sum());
+            
+            var knownValues = new List<int>();
+            var current = 0;
+            for (int ctr = 0; ctr < data.Count; ctr++)
+            {
+                current += data[ctr];
+                if (knownValues.Contains(current)) {
+                    Console.WriteLine($"Found duplicate: {current}");
+                    break;
+                } else {
+                    knownValues.Add(current);
+                }
+                if (ctr == data.Count - 1) {
+                    Console.WriteLine("Starting over");
+                    ctr = -1;
+                }
+            }
+
+
+        }
+    }
+}
